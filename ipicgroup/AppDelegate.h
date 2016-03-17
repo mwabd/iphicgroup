eebdd1d1664f720c297152b614b0940b84a76337
Mind Working Asia <MWA-21@Minds-Mac-mini-2.local>
2016-03-17 11:21:42 +0600

    Initial Commit

---
+++ ipicgroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ipicgroup
+//
+//  Created by Mind Working Asia on 3/17/16.
+//  Copyright (c) 2016 Mind Working Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ipicgroup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ipicgroup
+//
+//  Created by Mind Working Asia on 3/17/16.
+//  Copyright (c) 2016 Mind Working Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
